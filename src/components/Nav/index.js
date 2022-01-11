60fd5dbe821ddb3f7381cdfe693c48f9520fc70a
cshepscorp <sheppard.christy@gmail.com>
2022-01-10 20:07:58 -0500

    hooks added for stateChange and useEffect for category and title rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-function Nav() {
-    const Categories = [
-        {
-            name: "commercial",
-            description: "Photos of grocert stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies"},
-        {
-            name: "landscape",
-            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ];
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-      }
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+      }, [currentCategory]); // if currentCategory changes, re-render the component so that the change in document.title will be visible to the user
+
+
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 <a data-testid="link" href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
@@ -30,19 +27,21 @@ function Nav() {
                             About me
                         </a>
                     </li>
-                    <li>
+                    <li className="mx-2">
                         <span>Contact</span>
                     </li>
                     {/* Whenever we map over anything in JSX, the outermost element must have a key attribute that's set to be something unique. This helps React keep track of items in the virtual DOM */}
                     {/* When you map over an array in a JSX expression, you should return only a single JSX element—like how you can only return a single element from a React component. */}
-                    {Categories.map((category) => (
-                    <li className="mx-1"
-                        key={category.name} // we used category.name here bc we dont expect any categories to share the same name. id is often used as the key
+                    {categories.map((category) => (
+                    <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name} 
                     >
                         {/* The onClick() attribute is expecting a callback function declaration. It's important that we wrap it in a function declaration rather than just calling categorySelected(category.name), which would cause the function to get called whenever the component renders as well. */}
-                        <span onClick={() => categorySelected(category.name)} > 
-                        {/* // categorySelected is a function name */}
-                            {category.name}
+                        <span 
+                            onClick={() => {
+                                setCurrentCategory(category)
+                            }} 
+                        > 
+                            {capitalizeFirstLetter(category.name)}
                         </span>
                     </li>
                     ))}
